1c238f63f60f7ed97b26403fd7c2d5b410222b8b
karchx <kenethra.74@gmail.com>
2025-02-25 11:19:54 -0600

    feat: add CRUD empleados

--- constructora/empleados/api.py
+++ constructora/empleados/api.py
@@ -1,11 +1,10 @@
 import logging
 
-from flask import request, Response, jsonify
+from flask import request, jsonify
 from flask_appbuilder.api import expose, BaseApi
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from constructora.models.empleados import Empleado
 from constructora.empleados.schemas import EmpleadoPostSchema
-from constructora.extensions import db
 
 from marshmallow import ValidationError
 
@@ -33,5 +32,42 @@ class EmpleadosRestApi(BaseApi):
             self.datamodel.session.add(data)
             self.datamodel.session.commit()
             return item
+        except ValidationError as error:
+            return error.messages
+
+    @expose('/<int:id>', methods=("GET",))
+    def get(self, id):
+        try:
+            item = self.datamodel.session.query(Empleado). \
+                get_or_404(id)
+            return item.data
+        except ValidationError as error:
+            return error.messages
+
+    @expose('/<int:id>', methods=("PUT",))
+    def update(self, id):
+        try:
+            data_find = self.datamodel.session.query(Empleado). \
+                get_or_404(id)
+            item = self.add_model_schema.load(request.json, partial=True)
+
+            for key, value in item.items():
+                setattr(data_find, key, value)
+
+            self.datamodel.session.commit()
+
+            return data_find.data
+        except ValidationError as error:
+            return error.messages
+
+    @expose('/<int:id>', methods=("DELETE",))
+    def delete(self, id):
+        try:
+            record = self.datamodel.session.query(Empleado). \
+                filter_by(id=id).delete()
+
+            self.datamodel.session.commit()
+
+            return { "message": record }
         except ValidationError as error:
             return error.messages
